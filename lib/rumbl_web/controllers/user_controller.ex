29800ceaee75c5196a808ef3f614f8ea586035ca
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-05-25 11:21:44 -0600

    Manage videos and authenticate users

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,7 +1,7 @@
 defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
@@ -32,16 +32,5 @@ defmodule RumblWeb.UserController do
         render(conn, "new.html", changeset: changeset)
     end
 
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt()
-    end
   end
 end
